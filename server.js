afd99ae6365e1e24aff73a8bffa22857def165a4
rvbandgar <31121544+rvbandgar@users.noreply.github.com>
2017-08-21 11:37:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('Article one requested & will be served here');
+  res.send('Article two requested & will be served here');
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('Article one requested & will be served here');
+  res.send('Article three requested & will be served here');
 });
 
 
